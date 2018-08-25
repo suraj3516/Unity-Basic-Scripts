f63ee46445722133c5a565d5fab6832d80bf429e
Suraj Singh Rana <surajrana3516@gmail.com>
2018-08-25 19:43:34 +0530

    Add files via upload

---
+++ Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+	
+	public Text score;
+	//static variable can be accessed and modify through any class
+	public static int currScore ;
+
+
+	void Update(){
+		score.text = ("SCORE\n" + currScore.ToString());
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "rings") {
+			Destroy (col.gameObject);
+			currScore += 5;
+		}
+	}
+}
