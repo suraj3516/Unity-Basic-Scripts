d8da6f1a6806ce0fe6ee23b026cfeaa50c47969c
Suraj Singh Rana <surajrana3516@gmail.com>
2018-08-25 19:49:28 +0530

    Add files via upload
    For creating easy Laser using line renderer.

---
+++ laser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser : MonoBehaviour {
+	public LineRenderer l;
+	public Transform origin;
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+		l = GetComponent<LineRenderer> ();
+		l.startWidth= 5f;
+		l.endWidth = 5f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		l.SetPosition (0, origin.position);
+		l.SetPosition (1,target.position);
+
+	}
+}
