d9057c4bfc0293c0c63935fbbdba224e21e5c316
Suraj Singh Rana <surajrana3516@gmail.com>
2018-08-25 19:52:18 +0530

    Add files via upload
    Countdown Timer for the game.

---
+++ Time_Remaining.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Time_Remaining : MonoBehaviour {
+	public Text timeRemaining;
+	private int startTime=60;
+	void Start(){
+		StartCoroutine ("loseTime");
+	}
+	void Update () {
+		timeRemaining.text = ("Time Remaining\n" + startTime.ToString());
+		if (startTime <= 0) {
+			StopCoroutine ("loseTime");
+			timeRemaining.text = ("Time Up");
+			Restart ();
+		}
+	}
+	public void Restart(){
+		//WaitForSeconds (5);
+		StartCoroutine ("waiting");
+		SceneManager.LoadScene ("end");
+	}
+	IEnumerator loseTime(){
+		while(true){
+			yield return new WaitForSeconds (1);
+			startTime--;
+		}
+	}
+	IEnumerator waiting(){
+		yield return new WaitForSeconds (5f);
+	}
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.name=="LP"){
+			Debug.Log (startTime);
+			startTime = startTime + 10;
+			Debug.Log (startTime);
+			Update ();
+		}
+	}
+}
