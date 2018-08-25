84bb6c56ff530615a3ba0dc1707a7c5d1f909a03
Suraj Singh Rana <surajrana3516@gmail.com>
2018-08-25 19:34:13 +0530

    Add files via upload

---
+++ Health.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour {
+	public Image[] life ;
+	int count = 0;
+	// Update is called once per frame
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag != "rings" && col.gameObject.tag != "gameFinish") {
+			Destroy (life [count]);
+			count++;
+		}
+		if (count >= 3) {
+			SceneManager.LoadScene ("Menu");
+			Cursor.lockState = CursorLockMode.Confined;
+			Cursor.visible = true;
+		}
+	}
+}
